abbbfecc5c554050c42e662d4fb401ffc15e33ca
zty <tonalll@qq.com>
2016-12-23 17:30:09 +0800

    tpadmin--

---
+++ tpadmin--/runtime/temp/64d3d5c27e5d6c4a5da6a6d30ecc8131.php
@@ -0,0 +1,107 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:3:{s:75:"F:\wamp\www\tp\tpadmin\public/../application/admin\view\pub\loginframe.html";i:1482306707;s:74:"F:\wamp\www\tp\tpadmin\public/../application/admin\view\template\base.html";i:1482306707;s:85:"F:\wamp\www\tp\tpadmin\public/../application/admin\view\template\javascript_vars.html";i:1482306707;}*/ ?>
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="renderer" content="webkit|ie-comp|ie-stand">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
+    <meta http-equiv="Cache-Control" content="no-siteapp"/>
+    <title><?php echo \think\Config::get('site.title'); ?></title>
+    <link rel="Bookmark" href="__ROOT__/favicon.ico" >
+    <link rel="Shortcut Icon" href="__ROOT__/favicon.ico" />
+    <!--[if lt IE 9]>
+    <script type="text/javascript" src="__LIB__/html5.js"></script>
+    <script type="text/javascript" src="__LIB__/respond.min.js"></script>
+    <script type="text/javascript" src="__LIB__/PIE_IE678.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" type="text/css" href="__STATIC__/h-ui/css/H-ui.min.css"/>
+    <link rel="stylesheet" type="text/css" href="__STATIC__/h-ui.admin/css/H-ui.admin.css"/>
+    <link rel="stylesheet" type="text/css" href="__LIB__/Hui-iconfont/1.0.7/iconfont.css"/>
+    <link rel="stylesheet" type="text/css" href="__LIB__/icheck/icheck.css"/>
+    <link rel="stylesheet" type="text/css" href="__STATIC__/h-ui.admin/skin/default/skin.css" id="skin"/>
+    <link rel="stylesheet" type="text/css" href="__STATIC__/h-ui.admin/css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="__STATIC__/css/app.css"/>
+    <link rel="stylesheet" type="text/css" href="__LIB__/icheck/icheck.css"/>
+    
+    <!--[if IE 6]>
+    <script type="text/javascript" src="__LIB__/DD_belatedPNG_0.0.8a-min.js"></script>
+    <script>DD_belatedPNG.fix('*');</script>
+    <![endif]-->
+    <!--定义JavaScript常量-->
+<script>
+    window.THINK_ROOT = '<?php echo \think\Request::instance()->root(); ?>';
+    window.THINK_MODULE = '<?php echo \think\Url::build("/" . \think\Request::instance()->module(), "", false); ?>';
+    window.THINK_CONTROLLER = '<?php echo \think\Url::build("___", "", false); ?>'.replace('/___', '');
+</script>
+</head>
+<body>
+
+
+<div class="page-container">
+    <form class="form form-horizontal" id="form" method="post" action="<?php echo \think\Url::build('checkLogin'); ?>">
+        <div class="row cl">
+            <label class="form-label col-xs-3 col-sm-3"><span class="c-red">*</span>帐号：</label>
+            <div class="formControls col-xs-6 col-sm-6">
+                <input type="text" class="input-text" placeholder="帐号" name="account" datatype="*" nullmsg="请填写帐号">
+            </div>
+            <div class="col-xs-3 col-sm-3"></div>
+        </div>
+        <div class="row cl">
+            <label class="form-label col-xs-3 col-sm-3"><span class="c-red">*</span>密码：</label>
+            <div class="formControls col-xs-6 col-sm-6">
+                <input type="password" class="input-text" placeholder="密码" name="password" datatype="*" nullmsg="请填写密码">
+            </div>
+            <div class="col-xs-3 col-sm-3"></div>
+        </div>
+        <div class="row cl">
+            <label class="form-label col-xs-3 col-sm-3">验证码：</label>
+            <div class="formControls col-xs-6 col-sm-6">
+                <input name="captcha" class="input-text" type="text" placeholder="验证码" style="width:100px;" datatype="*" nullmsg="请填写验证码">
+                <img id="captcha" src="<?php echo captcha_src(); ?>" alt="验证码" title="点击刷新验证码" style="cursor:pointer;width: 150px;height: 40px">
+            </div>
+            <div class="col-xs-3 col-sm-3"></div>
+        </div>
+
+        <div class="row cl">
+            <div class="col-xs-8 col-sm-9 col-xs-offset-4 col-sm-offset-3">
+                <button type="submit" class="btn btn-primary radius">&nbsp;&nbsp;提交&nbsp;&nbsp;</button>
+                <button type="button" class="btn btn-default radius ml-20" onClick="layer_close();">&nbsp;&nbsp;取消&nbsp;&nbsp;</button>
+            </div>
+        </div>
+    </form>
+</div>
+
+<script type="text/javascript" src="__LIB__/jquery/1.9.1/jquery.min.js"></script>
+<script type="text/javascript" src="__LIB__/layer/2.4/layer.js"></script>
+<script type="text/javascript" src="__STATIC__/h-ui/js/H-ui.js"></script>
+<script type="text/javascript" src="__STATIC__/h-ui.admin/js/H-ui.admin.js"></script>
+<script type="text/javascript" src="__STATIC__/js/app.js"></script>
+<script type="text/javascript" src="__LIB__/icheck/jquery.icheck.min.js"></script>
+
+<script type="text/javascript" src="__LIB__/Validform/5.3.2/Validform.min.js"></script>
+<script>
+    $(function () {
+        $("#captcha").click(function () {
+            $(this).attr("src","<?php echo captcha_src(); ?>?t="+new Date().getTime())
+        });
+
+        $("#form").Validform({
+            tiptype:2,
+            ajaxPost:true,
+            showAllError:true,
+            callback:function(ret){
+                ajax_progress(ret);
+                if (ret.status == 'n'){
+                    if (ret.info == '验证码错误!'){
+                        $("#captcha").click();
+                    }
+                }
+            }
+        });
+    })
+</script>
+
+</body>
+</html>
